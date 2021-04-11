b89a8f0c3f410541c341c640271292919c98b78b
jixintong813 <81578568+jixintong813@users.noreply.github.com>
2021-04-11 23:16:50 +0800

    Update_Assignment1_xintong (#24)
    * Create Assignment1-纪昕彤.ipynb

---
+++ week1-introduction/assignment1/Assignment1-xintong.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Assignment 1
+# This assignment is for exercising Python fundamental I and getting familiar with Python syntax.
+
+# 注意 - Copy this file and rename as assignment1-{first_name}.py then complete code with a PR.
+# 注意 - Copy this file and rename as assignment1-{first_name}.py then complete code with a PR.
+# 注意 - Copy this file and rename as assignment1-{first_name}.py then complete code with a PR.
+
+# Q1. Write a program which can compute the factorial of a given numbers.
+
+
+def factorial(x: int) -> int:
+    if x < 1:
+        return -1
+    else:
+        for i in range(1,x + 1):
+            result *= i
+    return result
+        
+    
+assert factorial(0) == 1
+assert factorial(1) == 1
+assert factorial(9) == 362880
+
+
+# Q2. Write a program which take a num and print a str as the sum of all numbers from 1 to this number
+# [1 + 2 + ... + x] and x is always >= 1.
+
+def print_sum(x: int) -> str:
+    return str(_sum(x))
+
+def _sum(x):
+    if x == 1:
+        return 1
+    return x + _sum(x - 1)
+
+
+assert print_sum(1) == "1"
+assert print_sum(3) == "6"
+assert print_sum(5) == "15"
+
+
+# Q3. Write a program to check is a year is leap year (x is always > 0)
+
+def is_leap_year(year: int) -> bool:
+    return year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
+
+
+assert is_leap_year(2000)
+assert is_leap_year(1996)
+assert not is_leap_year(1900)
+assert not is_leap_year(2001)
+
+
+# Q4. Write a program to convert a list of lowercase words to uppercase words.
+
+def to_upper_case(words: [str]) -> [str]:
+    w = list(map(str.upper,words))
+    return w
+
+
+assert to_upper_case(["abc", "de"]) == ["ABC", "DE"]
+assert to_upper_case(["Amazon", "Apple"]) == ["AMAZON", "APPLE"]
+
+
+# Q5. Write a program to use only 'and' and 'or' to implement 'xor'
+# https://baike.baidu.com/item/%E5%BC%82%E6%88%96/10993677?fromtitle=xor&fromid=64178
+
+def xor(a: bool, b: bool) -> bool:
+    return a != b
+
+
+assert not xor(True, True)
+assert xor(True, False)
+assert xor(False, True)
+assert not xor(False, False)
